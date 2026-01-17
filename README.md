c8a02cac1115b86991b45ecf3589ceb93f538097
gautamapurva <gautamapurva110807.nancy@gmail.com>
2026-01-17 14:27:38 +0530

    Took inputs and made cases

--- README.md
+++
@@ -1,9 +0,0 @@
-# SIMPLE COMMAND LINE CALCULATOR
-def add(x,y): return x+y
-def subtract(x,y): return x-y
-def multiply(x,y): return x*y
-def divide(x,y): 
-if(y!=0):
-     return x/y
-else:
-    print("Error:divide by zero")
---
+++ README.py
@@ -0,0 +1,28 @@
+# SIMPLE COMMAND LINE CALCULATOR
+def add(x,y): 
+     return x+y
+def subtract(x,y): 
+     return x-y
+def multiply(x,y): 
+     return x*y
+def divide(x,y): 
+     if(y!=0):
+          return x/y
+     else:
+          print("Error:divide by zero")
+print("Select operation:")
+print("1.Add\n 2.Sub\n 3.Mul\n 4.Div")
+
+while True:
+     choice= input("Enter choice (1/2/3/4): ")
+     if choice in('1','2','3','4'):
+          num1= float(input("Enter first number:"))
+          num2= float(input("Enter second number:"))
+          if choice=='1':
+               print("result:",add(num1,num2))
+          elif choice=='2':
+               print("result:",subtract(num1,num2))
+          elif choice=='3':
+               print("result:",multiply(num1,num2))
+          elif choice=='4':
+               print("result:",divide(num1,num2))
